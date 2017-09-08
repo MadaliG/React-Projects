a816e861ca16f837d55ca73626650272a7d1ccf9
Madalina Grigoras <madalinag@advahoo.local>
2017-09-08 18:37:32 +0300

    modified button

--- my-app/HomeScreen.js
+++ my-app/HomeScreen.js
@@ -5,7 +5,6 @@ import {
   Button,
   Text,
 } from 'react-native';
-import SimpleButton from './SimpleButton';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = (({ navigation }) => ({
@@ -16,16 +15,11 @@ export default class HomeScreen extends React.Component {
   const {navigate} = this.props.navigation; 
   return (
      <View style={styles.container}>
-		<Text style={styles.noNotesText}>You haven't created any notes!</Text>
         <Text>Hello, Notes App!</Text>
-        <SimpleButton
-			onPress={() => this.props.navigator.push({
-			name: 'createNote'
-			})}
-			customText="Create Note"
-			style={styles.simpleButton}
-			textStyle={styles.simpleButtonText}
-		/>
+        <Button
+          onPress={()=> navigate('Note', { user: 'Mark' })}
+          title="Create a note!"
+        />
       </View>
     );
   }
@@ -34,35 +28,12 @@ export default class HomeScreen extends React.Component {
 
 
 
-var styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-	noNotesText: {
-		color: '#48209A', //#778899
-		marginBottom: 10
-	},
-	simpleButton: {
-		backgroundColor: '#5B29C1', //#B0C4DE
-		borderColor: '#48209A', //#778899
-		borderWidth: 1,
-		borderRadius: 4,
-		paddingHorizontal: 20,
-		paddingVertical: 15,
-		shadowColor: 'darkgrey',
-		shadowOffset: {
-			width: 1,
-			height: 1
-		},
-		shadowOpacity: 0.8,
-		shadowRadius: 1,
-	},
-	simpleButtonText: {
-		color: 'white',
-		fontWeight: 'bold',
-		fontSize: 16
-	}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
 
